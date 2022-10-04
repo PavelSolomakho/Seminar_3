e90a76269941af5f25271bed7f6f0ccd961326a8
Pavel <archangel-44@mail.ru>
2022-10-04 22:04:07 +0300

    5 задача

--- DZ-3zadacha5.py
+++ DZ-3zadacha5.py
@@ -20,7 +20,7 @@ def NegaFibonacci(n):
         return num2
 
 list = [0]
-userNumber = int(input('Enter any number: '))
+userNumber = int(input('Введите любое число: '))
 for e in range(1, userNumber + 1):
     list.append(Fibonacci(e))
     list.insert(0, NegaFibonacci(e))
